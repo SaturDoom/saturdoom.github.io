974e806ec4ef4e964ae8236b12cecc2091153b34
M1st3rfr10 <el mail que uss en GitHub>
2026-04-18 14:31:11 -0600

    WIP: adjust CSS variables usage

--- script.js
+++ script.js
@@ -148,7 +148,7 @@
     ctx.translate(width / 2, height / 2 + yOffset);
     ctx.rotate(-Math.PI / 2);
     
-    const radius = Math.min(width, height) * 0.85;
+    const radius = Math.min(width, height) * 1;
     const centerX = 0;
     const centerY = 0;
     const startAngle = -Math.PI / 2;
